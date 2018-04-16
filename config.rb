e4541fff2b1f01d32e026d50e155b2f5f25a9784
Elliot <elbelet@gmail.com>
2018-04-17 01:02:23 +0200

    initial commit

---
+++ config.rb
@@ -0,0 +1,12 @@
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "sass"
+css_dir = "css"
+images_dir = "img"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+line_comments = false
+preferred_syntax = :sass
+output_style = :expanded # option = expanded, nested, compact(default), compressed
+relative_assets = true
+sourcemap = true
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Readme
+* [Markdown CheatSheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
